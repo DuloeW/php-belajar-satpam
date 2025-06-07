0a2c0551196e54da5e82b27efd6b4987a416ba80
DuloeW <duloekeren@gmail.com>
2025-06-07 09:01:59 +0800

    Adding varibel env

--- auth/koneksi.php
+++ auth/koneksi.php
@@ -1,14 +1,14 @@
 <?php
-$host = "localhost";
-$username = "root";
-$password = ""; 
-$database = "kampus";
+$env = parse_ini_file(__DIR__ . '/../.env');
+
+$host = $env['HOST'];
+$username = $env['USERNAME'];
+$password = $env['PASSWORD'];
+$database = $env['DATABASE'];
 
 $koneksi = mysqli_connect($host, $username, $password, $database);
 
-// Periksa koneksi
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
-
 ?>
